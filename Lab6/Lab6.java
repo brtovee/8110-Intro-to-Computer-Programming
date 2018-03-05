9ca3a8719c1d584683be6c23feb27f3e1ef7800a
Bruce Tovee <37085952+brtovee@users.noreply.github.com>
2018-03-05 17:06:55 -0500

    Update Lab6.java

--- Lab6/Lab6.java
+++ Lab6/Lab6.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
-
+/* Author: Bruce Tovee
+* Asking for 2 number and finding the greatest common factor
+*/
 public class Lab6 {
+	//variables
 	private int value1, value2, gcf;
 	Scanner input = new Scanner (System.in);
 	
@@ -8,8 +11,9 @@ public class Lab6 {
 		value1 = 0;
 		value2 = 0;
 		boolean loop = true;
-		
+		//value 1 input
 		System.out.print("Enter value1: ");
+		//error checking to makes sure value1 is greater than 0
 		while(loop) {
 			value1 = input.nextInt();
 			if(value1 <= 0) {
@@ -21,7 +25,9 @@ public class Lab6 {
 		}
 		
 		loop = true;
+		//value2 input
 		System.out.print("Enter value2: ");
+		//error checking to make sure value 2 is greater than 0
 		while(loop) {
 			value2 = input.nextInt();
 			if(value2 <= 0) {
@@ -34,7 +40,7 @@ public class Lab6 {
 
 	}
 	
-	public void calculateGcf() {
+	public void calculateGcf() { // calculating the greatest common factor
 		int a = value1;
 		int b = value2;
 		int temp;
@@ -45,7 +51,7 @@ public class Lab6 {
 		 }
 		 gcf = a;
 	}
-	public void display() {
+	public void display() { //displaying both values and greatest common factor
 		System.out.println("The greatest common factor of " + value1 + " and "+ value2 +" is " +gcf);
 	}
 	
